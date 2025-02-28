6502c1a07667aadf4fcfb29d63202967c36f050b
patakchiyousefi1 <k.patakchi.yousefi@fz-juelich.de>
2025-02-28 11:43:00 +0100

    -L flag in cdo - updated HRES_POST2.sh

--- run_HRES_POST.sh
+++ run_HRES_POST.sh
@@ -7,7 +7,7 @@
 #SBATCH --error=LOGS/HRES_POST_job.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=01:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=batch
 #SBATCH --mail-user=k.patakchi.yousefi@fz-juelich.de
 #SBATCH --mail-type=ALL
@@ -21,8 +21,8 @@ if [ "$RUN_FIRST_PART" = true ]; then
 
     rm $HRES_DUMP/*
     rm $HRES_DUMP2/*
-    rm $HRES_DUMP3/* 
-    rm $HRES_DUMP4/* 
+    rm -rf $HRES_DUMP3 && mkdir $HRES_DUMP3
+    rm -rf $HRES_DUMP4 && mkdir $HRES_DUMP4
     
     # run the first part of post-processing:
     source HRES_POST.sh
@@ -36,13 +36,13 @@ if [ "$RUN_SECOND_PART" = true ]; then
     rm $HRES_POST/*
 
     script="/p/project/cesmtst/patakchiyousefi1/CODES-MS3/FORECASTLEAD/HRES_POST2.sh"
-    for year in {2018..2018}; do
-        for month in {01..01}; do
+    for year in {2018..2023}; do
+        for month in {01..12}; do
             start="${year}${month}01 13"
             job_name="HRES_POST_$(date -d "$start" "+%Y%m%d_%H%M%S")"
             while true; do
                 num_jobs=$(squeue -u patakchiyousefi1 | wc -l)
-                if ((num_jobs <= 5)); then
+                if ((num_jobs <= 8)); then
                     echo "submitted $start job!"
                     sbatch <<EOL
 #!/bin/bash
@@ -71,4 +71,4 @@ EOL
 #rm -r $HRES_DUMP/* $HRES_DUMP2/* $HRES_DUMP3/* $HRES_DUMP4/*
 
 fi
-exit
+
